6c7c3a496d915d28ed50edd8eb805fba9e799a3c
diaz <carlosd2mil@gmail.com>
2026-02-20 22:29:41 -0600

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module commuteboard
+
+go 1.25.6
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+type Location struct {
+	name      string
+	latitude  string
+	longitude string
+}
+
+func main() {
+	fmt.Println("Hello")
+}
